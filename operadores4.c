22240fbc6dc662a9f31b34c0bd2b21e937587454
brichterf <bere.richter@gmail.com>
2025-03-18 22:13:19 +0000

    opa5

---
+++ operadores4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+int numero1 = 1, resultado;
+
+printf("Antes do incremento: %d\n", numero1);
+ //Pós incremento seria:
+ //resultado = numero1;
+ //numero1++;
+resultado = numero1++;
+printf("Após o Pós-incremento - Número 1: %d - Resultado: %d\n", numero1, resultado);
+
+resultado = ++numero1;
+printf("Após o Pré-incremento - Número 1: %d - Resultado: %d\n", numero1, resultado);
+
+return 0;
+
+}
