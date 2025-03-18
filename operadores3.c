829b6a89b0eb4a259f3e49b7a97f38488ca0e096
brichterf <bere.richter@gmail.com>
2025-03-18 16:23:49 +0000

    oparitméticos4

---
+++ operadores3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+int numero1 = 1;
+
+printf("Antes do incremento: %d\n", numero1);
+ //numero1 = numero1 + 1;
+numero1++;
+printf("Após o incremento: %d\n", numero1);
+
+ //numero1 = numero 1 - 1;
+numero1--;
+printf("Após o decremento: %d\n", numero1);
+
+return 0;
+
+}
